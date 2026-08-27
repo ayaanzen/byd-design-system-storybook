b0c4d3473a627b2e8c685f4499756332958565e9
Ayaan <ayaan@example.com>
2026-08-27 16:05:59 +0530

    chore: fix UltraCarousel title in Storybook

--- src/components/UltraCarousel/UltraCarousel.stories.tsx
+++ src/components/UltraCarousel/UltraCarousel.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { UltraCarousel } from './UltraCarousel';
 
 const meta: Meta<typeof UltraCarousel> = {
-  title: 'Figma Library/Component 23',
+  title: 'Figma Library/UltraCarousel',
   component: UltraCarousel,
   argTypes: {
     property1: { control: 'select', options: ['Frame 28', 'Frame 29', 'Frame 30', 'Frame 31', 'Frame 32'] }
