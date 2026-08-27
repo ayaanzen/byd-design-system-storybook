ef0bfed6813f713915c529c4819956324f6d0046
Ayaan <ayaan@example.com>
2026-08-27 16:05:33 +0530

    chore: actually fix storybook sidebar titles

--- update_titles.js
+++ update_titles.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const srcDir = path.join(process.cwd(), 'src', 'components');
-
-const dirs = fs.readdirSync(srcDir);
-dirs.forEach(dir => {
+fs.readdirSync(srcDir).forEach(dir => {
   const storyFile = path.join(srcDir, dir, dir + '.stories.tsx');
   if (fs.existsSync(storyFile)) {
     let content = fs.readFileSync(storyFile, 'utf8');
-    // Replace 'Figma Library/Component X' with 'Figma Library/DirName'
-    content = content.replace(/title: 'Figma Library\/.*'/, 	itle: 'Figma Library/ + dir + ');
+    content = content.replace(/title: 'Figma Library.*',/g, 'title: \\'Figma Library/' + dir + '\\',');
     fs.writeFileSync(storyFile, content, 'utf8');
   }
 });
-console.log('Updated story titles');
+console.log('Fixed titles');
